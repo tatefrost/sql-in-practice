e9ba358b84e837eb27e2c52bf23737814c1a8c97
Tate Frost <tatefrost96@gmail.com>
2021-12-08 16:26:38 -0700

    fixed everything

--- server/controller.js
+++ server/controller.js
@@ -16,7 +16,7 @@ let nextEmp = 5
 
 module.exports = {
     getUpcomingAppointments: (req, res) => {
-        sequelize.query(`select a.appt_id, a.date, a.service_type, a.approved, a.completed, u.first_name, u.last_name 
+        sequelize.query(`select a.appt_id, a.date, a.service_type, a.approved, a.completed, a.notes, u.first_name, u.last_name 
         from cc_appointments a
         join cc_emp_appts ea on a.appt_id = ea.appt_id
         join cc_employees e on e.emp_id = ea.emp_id
@@ -30,7 +30,7 @@ module.exports = {
     approveAppointment: (req, res) => {
         let {apptId} = req.body
     
-        sequelize.query(`UPDATE appointments SET approved = true WHERE appt_id = ${apptId}
+        sequelize.query(`UPDATE cc_appointments SET approved = true WHERE appt_id = ${apptId};
         
         insert into cc_emp_appts (emp_id, appt_id)
         values (${nextEmp}, ${apptId}),
@@ -69,7 +69,7 @@ module.exports = {
     }, 
 
     completeAppointment: (req, res) => {
-        sequelize.query(`UPDATE appointments SET completed = true WHERE appt_id = ${apptId};`)
+        sequelize.query(`UPDATE cc_appointments SET completed = true WHERE appt_id = ${req.body.apptId};`)
             .then(dbRes => res.status(200).send(dbRes[0]))
             .catch(err => console.log(err))
     }
